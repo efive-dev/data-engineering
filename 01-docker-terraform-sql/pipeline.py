ba8cab2fd800b8ec4e2c2e5d4b22abf40438bf22
efive-dev <edoardo.quinto.dev@gmail.com>
2025-09-07 22:48:20 +0200

    docker and postgres

---
+++ 01-docker-terraform-sql/pipeline.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+
+print(sys.argv)
+day = sys.argv[1]
+
+# docker run -it   -e POSTGRES_USER="root"   -e POSTGRES_PASSWORD="root"   -e POSTGRES_DB="ny_taxi"   -v ./ny_taxi_postgres_data:/var/lib/postgresql/data   -p 5432:5432   postgres:13
+# some fancy stuff with pandas
+
+print(f'job finished successfully for day: {day}')
